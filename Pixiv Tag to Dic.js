c80f87e4279af711bf84f65d258a8913f0f255dd
Xinyi Rong <Lizard.rar@gmail.com>
2020-05-16 02:33:35 -0400

    Enhance Pixiv Tag to Dic.js

--- Pixiv Tag to Dic.js
+++ Pixiv Tag to Dic.js
@@ -2,13 +2,13 @@
 // @name         Pixiv Tag to Dic
 // @namespace    https://github.com/Xdynix/Tampermonkey-Scripts
 // @homepage     https://github.com/Xdynix/Tampermonkey-Scripts
-// @version      0.1.2
+// @version      0.2.0
 // @description  Add link from Pixiv tag page to dictionary page.
 // @author       Xdynix
 // @updateURL    https://github.com/Xdynix/Tampermonkey-Scripts/raw/master/Pixiv%20Tag%20to%20Dic.js
 // @downloadURL  https://github.com/Xdynix/Tampermonkey-Scripts/raw/master/Pixiv%20Tag%20to%20Dic.js
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
-// @match        https://www.pixiv.net/tags/*
+// @match        https://www.pixiv.net/*
 // @grant        none
 // ==/UserScript==
 
@@ -26,19 +26,22 @@
         }, interval);
     }
 
-    const tagNameMatch = location.pathname.match(/\/tags\/(.+?)\/.+/i);
-    if (tagNameMatch !== null) {
-        const tagName = decodeURIComponent(tagNameMatch[1]);
-        loopApplyIfMatch(function (elements, loop) {
-            const novelButton = $(elements[0]);
-            const dicButton = novelButton.clone();
-            dicButton.attr({
-                href: `https://dic.pixiv.net/a/${tagName}`,
-                target: '_blank',
-            });
-            $(':first-child', dicButton).text('词典');
-            dicButton.insertAfter(novelButton);
-            clearInterval(loop);
-        }, `nav > a:nth-last-child(2)[href^='/tags/${tagName}']`);
-    }
+    setInterval(function () {
+        const tagNameMatch = location.pathname.match(/\/tags\/(.+?)\/.+/i);
+        if (tagNameMatch !== null && !document.getElementById('dic-button')) {
+            const tagName = decodeURIComponent(tagNameMatch[1]);
+            loopApplyIfMatch(function (elements, loop) {
+                const novelButton = $(elements[0]);
+                const dicButton = novelButton.clone();
+                dicButton.attr({
+                    href: `https://dic.pixiv.net/a/${tagName}`,
+                    target: '_blank',
+                    id: 'dic-button'
+                });
+                $(':first-child', dicButton).text('词典');
+                dicButton.insertAfter(novelButton);
+                clearInterval(loop);
+            }, `nav > a:nth-last-child(2)[href^='/tags/${tagName}']`);
+        }
+    }, 100);
 })();
