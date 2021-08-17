f810032e5953a31b85e3aecc8cb9dc09047d06c4
Xdynix <Lizard.rar@gmail.com>
2021-08-17 02:47:56 -0700

    Add NGA free copy code

--- src/Free Copy.js
+++ src/Free Copy.js
@@ -2,7 +2,7 @@
 // @name         Free Copy
 // @namespace    https://github.com/Xdynix/Tampermonkey-Scripts
 // @homepage     https://github.com/Xdynix/Tampermonkey-Scripts
-// @version      0.1.6
+// @version      0.2.0
 // @description  Enable copying on some sites.
 // @author       Xdynix
 // @updateURL    https://github.com/Xdynix/Tampermonkey-Scripts/raw/master/src/Free%20Copy.js
@@ -12,6 +12,9 @@
 // @match        *://www.anitama.cn/*
 // @match        *://www.bilibili.com/*
 // @match        *://www.zhihu.com/*
+// @match        *://bbs.nga.cn/*
+// @match        *://ngabbs.com/*
+// @match        *://nga.178.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -21,9 +24,10 @@
     const $ = window.jQuery.noConflict(true);
     const url = new URL(window.location.href);
 
-    function onCopy(e) {
+    function onCopy(e, textHook) {
         const clipboardData = window.clipboardData || e.originalEvent.clipboardData;
-        clipboardData.setData('text/plain', window.getSelection().toString());
+        const rawText = window.getSelection().toString();
+        clipboardData.setData('text/plain', textHook === undefined ? rawText : textHook(rawText));
         e.preventDefault();
         e.stopPropagation();
     }
@@ -41,7 +45,7 @@
         $('#area-content-article').on('copy', onCopy);
     }
     if (url.host === 'www.bilibili.com' && url.pathname.startsWith('/read')) {
-        setTimeout(function () {
+        setTimeout(() => {
             $(document).on('copy', onCopy);
             $('#article-content').on('copy', onCopy);
             $('.unable-reprint').css('user-select', 'text');
@@ -50,6 +54,10 @@
     if (url.host === 'www.zhihu.com') {
         $(document).on('copy', onCopy);
     }
+    if (url.host === 'bbs.nga.cn' || url.host === 'ngabbs.com' || url.host === 'nga.178.com') {
+        const stripTail = s => s.replace(/\n\nhttps:\/\/.*read\.php\?.*$/gm, '');
+        $('.ubbcode').on('copy', e => onCopy(e, stripTail));
+    }
 
-    console.log("Freedom!!!");
+    console.log('Freedom!!!');
 })();
